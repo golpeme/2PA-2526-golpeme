1e7ada47b83c36f5de8ab33c35ca7ecad1b05ac5
golpeme <golpeme@esat-alumni.com>
2025-10-07 09:37:30 +0200

    Add files via upload

---
+++ Parcheesi/main.cc
@@ -0,0 +1,20 @@
+//#include porfa
+#include "parcheesi.cc"
+
+int main(){
+  Parcheesi parchis{4};  //Instancia
+
+
+  srand(time(NULL));
+
+ 
+  bool win = false;
+  do{
+
+  }while(!win);
+
+  
+  return 0;
+}
+
+    
